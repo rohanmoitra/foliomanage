13dfc6d0bffa79d3a923ee31df52b1af515f3725
Rohan Moitra <rohanmoitra@gmail.com>
2012-10-22 22:07:34 -0400

    added ui elements and provided some more info for the stocks

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,9 +1,44 @@
+require 'open-uri'
+require 'csv'
 class StocksController < ApplicationController
   # GET /stocks
   # GET /stocks.json
   def index
     @stocks = Stock.all
+    query_string = ""
+    @stocks.each do |stock|
+      query_string = query_string + "+" + stock.symbol
+    end
+    url = "http://finance.yahoo.com/d/quotes.csv?s=#{query_string}&f=nl1c1"
+    csv = CSV.parse(open(url).read)
+    @result = Array.new
+    #parse csv data
+    csv.each do |row|
+      puts row
+      @result += row
+    end
+    @counter=0
+    count=0
+    @hash = Hash.new
+    @stocks_array=Array.new
 
+    @stocks.each do |stock|
+      @hash["symbol"] = stock.symbol
+      @hash["name"]= stock.name
+      count=count+1
+      @hash["holding"]= stock.holding
+      @hash["price"]= stock.price
+      @hash["current_price"]= @result[count]
+      count=count+1
+      @hash["daily_gain"]=stock.holding.to_f * @result[count].to_f
+      count=count+1
+      @hash["overall_gain"]=(stock.price.to_f-stock.price.to_f) * stock.holding.to_f
+      @hash["current_value"]=@hash["current_price"].to_f * stock.holding.to_f
+      @total_value = @total_value.to_f + @hash["current_value"].to_f
+      @total_investment = @total_investment.to_f + (stock.price.to_f * stock.holding.to_f)
+      @stocks_array.push(@hash)
+    end
+    @total_gain=@total_value.to_f-@total_investment.to_f
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @stocks }
