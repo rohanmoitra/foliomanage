e9ef5ce093dab367913ea02f0a00fbf8934da567
Rohan Moitra <rohanmoitra@gmail.com>
2012-10-23 01:36:15 -0400

    some login validations

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,7 +5,7 @@ require 'csv'
 class StocksController < ApplicationController
 
   
-  skip_before_filter :authorize, :only => [:new, :create]
+  #skip_before_filter :authorize, :only => [:new, :create]
   # GET /stocks
   # GET /stocks.json
   def index
